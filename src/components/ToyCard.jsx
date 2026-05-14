520a946af8ca53d5416e10cca60ce037b65dc1d0
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-14 13:17:54 +0300

    added like toy feature and fixed ToyForm bug to make the test run correct

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function ToyCard({ toy, handleDelete }) {
+function ToyCard({ toy, handleDelete, handleLike }) {
   return (
     <div className="card" data-testid="toy-card">
       <h2>{toy.name}</h2>
       <img src={toy.image} alt={toy.name} className="toy-avatar" />
       <p>{toy.likes} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
+      <button className="like-btn" onClick={() => handleLike(toy.id)}>
+        Like {"<3"}
+      </button>
       <button className="del-btn" onClick={() => handleDelete(toy.id)}>
         Donate to GoodWill
       </button>
