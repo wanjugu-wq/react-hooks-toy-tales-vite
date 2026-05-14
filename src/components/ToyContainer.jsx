e009508bcb0f7024c607b7f85fce26abdd4ee5d2
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-14 12:56:11 +0300

    added delete feature

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ToyCard from "./ToyCard";
 import { useState, useEffect } from "react";
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, handleDelete }) {
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard key={toy.id} toy={toy} />
+        <ToyCard key={toy.id} toy={toy} handleDelete={handleDelete} />
       ))}
     </div>
   );
