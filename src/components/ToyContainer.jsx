73648c3557e399dcf2e68a724525495dcee8c352
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-14 12:15:28 +0300

    added display feature

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ToyCard from "./ToyCard";
+import { useState, useEffect } from "react";
 
-function ToyContainer() {
+function ToyContainer({ toys }) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))}
+    </div>
   );
 }
 
